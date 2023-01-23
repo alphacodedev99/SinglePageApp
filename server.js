fc8bbe44e49863c464d9f2e5bd6373ea8568b5a2
AlphaDev <30984729+AlphaForza@users.noreply.github.com>
2023-01-23 21:02:55 +0100

    backend

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.listen(3000, () => {
+	console.log('====================================');
+	console.log('RADI SRW');
+	console.log('====================================');
+});
